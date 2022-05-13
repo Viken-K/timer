315b14c5964ea5d2670c081fada025afc7b45f07
Viken-K <60818826+Viken-K@users.noreply.github.com>
2022-05-13 19:24:51 +0800

    Add ZH translation

--- timer-selector.js
+++ timer-selector.js
@@ -213,7 +213,7 @@ function buildUrl(language, startDate, durationMs, progressMessage, finishedMess
     parameters.append('style', styling);
   }
 
-  return 'https://scpwiki.github.io/timer/timer.html?' + parameters;
+  return 'https://viken-k.github.io/timer/timer.html?' + parameters;
 }
 
 function buildWikitext(template, url, height, width) {
